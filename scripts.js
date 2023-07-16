cbafe24eb66eee578be5bbea7c1ba0c4964c79ae
David Valencia <davidvalencia0526@gmail.com>
2023-07-16 01:37:41 -0500

    incluida funcion de colores consistentes

--- scripts.js
+++ scripts.js
@@ -101,7 +101,7 @@ function createSubject() {
     return;
   }
 
-  horarios.addSubject(newSubjectName, generatePastelColor(), subjectCredits);
+  horarios.addSubject(newSubjectName, generatePastelColor(newSubjectName), subjectCredits);
 
   updateSubjectsAndSchedules();
 
@@ -590,10 +590,36 @@ function createScheduleTable() {
   return table;
 }
 
-function generatePastelColor() {
-  const hue = Math.floor(Math.random() * 360);
-  const saturation = Math.floor(Math.random() * 20) + 80;
-  const lightness = Math.floor(Math.random() * 20) + 70;
+//parte de generar colores 
+
+function hashCode(str) {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  return hash;
+}
+
+// Función para convertir un número entero en un valor de tono (hue) entre 0 y 360
+function intToHue(i) {
+  return i % 360;
+}
+
+// Función para convertir un número entero en un valor de saturación entre 80 y 100
+function intToSaturation(i) {
+  return (i % 21) + 80;
+}
+
+// Función para convertir un número entero en un valor de luminosidad entre 70 y 90
+function intToLightness(i) {
+  return (i % 21) + 70;
+}
+
+function generatePastelColor(seedText) {
+  const hash = hashCode(seedText);
+  const hue = intToHue(hash);
+  const saturation = intToSaturation(hash);
+  const lightness = intToLightness(hash);
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
 }
 
