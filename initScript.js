1fca8aa538d68a13fb96e74ad639c46fb826878b
David Valencia <davidvalencia0526@gmail.com>
2024-01-12 16:55:24 -0500

    simplifique el initScript

--- initScript.js
+++ initScript.js
@@ -7,26 +7,32 @@ import { createInitialTable } from "./createTables.js";
 import { createSubject } from "./UI.js";
 import { saveToFile, loadFromFile } from "./files.js";
 
-export function initApp() {
+function initListeners(subjectManager) {
+  const createSubjectButton = document.getElementById("createSubjectButton");
+  const saveToFileButton = document.getElementById("saveToFileButton");
+  const fileInput = document.getElementById("fileInput");
+  const generateCombinedSchedulesButton = document.getElementById(
+    "generateCombinedSchedulesButton"
+  );
+  const toggleConflicts = document.getElementById("toggleConflicts");
+
+  createSubjectButton.addEventListener("click", () =>
+    createSubject(subjectManager)
+  );
+  saveToFileButton.addEventListener("click", () => saveToFile(subjectManager));
+  fileInput.addEventListener("change", () => loadFromFile(subjectManager));
+  generateCombinedSchedulesButton.addEventListener("click", () =>
+    generateCombinedSchedules(subjectManager)
+  );
+  toggleConflicts.addEventListener("change", toggleConflictSchedules);
+}
+
+function initApp() {
   const horario = new SubjectManager();
   createInitialTable(horario);
 
   document.addEventListener("DOMContentLoaded", () => {
-    const createSubjectButton = document.getElementById("createSubjectButton");
-    const saveToFileButton = document.getElementById("saveToFileButton");
-    const fileInput = document.getElementById("fileInput");
-    const generateCombinedSchedulesButton = document.getElementById(
-      "generateCombinedSchedulesButton"
-    );
-    const toggleConflicts = document.getElementById("toggleConflicts");
-
-    createSubjectButton.addEventListener("click", () => createSubject(horario));
-    saveToFileButton.addEventListener("click", () => saveToFile(horario));
-    fileInput.addEventListener("change", () => loadFromFile(horario));
-    generateCombinedSchedulesButton.addEventListener("click", () =>
-      generateCombinedSchedules(horario)
-    );
-    toggleConflicts.addEventListener("change", toggleConflictSchedules);
+    initListeners(horario);
   });
 }
 
